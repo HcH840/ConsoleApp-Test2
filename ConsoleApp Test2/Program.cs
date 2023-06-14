2292063f4d05c6fc96e827cd67ff294e56aa4d6b
WorkGroup-2014 <WorkGroup-2014@WORKGR-2016>
2023-06-14 06:04:13 -0800

    Add project files.

---
+++ ConsoleApp Test2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Enter a value of X: "); // Display String Reminder
+int x = Convert.ToInt32(Console.Readline()); // Similar to Scanner class (Read an input from the user)
+Console.Write("Enter a Value of Y: ");
+int y = Convert.ToInt32(Console.Readline());
+if (x + y == 14) then Console.WriteLine("Hello From C#") // If condition is Right, Write Hello World on the screen
+else
+if ((x + y != 14) && (x != y)) then Console.WriteLine("Wrong Answer!") // Do otherwise if the above is wrong
